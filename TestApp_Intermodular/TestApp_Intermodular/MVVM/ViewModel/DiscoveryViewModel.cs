4d60c4eeaca18e369395f32a2c6be8f5eb42ad45
Sergio Such <sergio.suchpico@gmail.com>
2023-01-14 19:44:02 +0100

    Añadido primera version de loggin y desarrollando funcionalidad de mapa

--- TestApp_Intermodular/TestApp_Intermodular/MVVM/ViewModel/DiscoveryViewModel.cs
+++ TestApp_Intermodular/TestApp_Intermodular/MVVM/ViewModel/DiscoveryViewModel.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.Integration;
+using TestApp_Intermodular.MVVM.View;
 
 namespace TestApp_Intermodular.MVVM.ViewModel
 {
-    class DiscoveryViewModel
+    class DiscoveryViewModel : DiscoveryView
     {
+        public DiscoveryViewModel()
+        {
+            WindowsFormsHost windowsFormsHost = new WindowsFormsHost();
+            this.Content = windowsFormsHost;
+            var webBrowser = new WebBrowser();
+            windowsFormsHost.Child = webBrowser;
+            webBrowser.Navigate("https://maps.google.com/maps?q=New+York&t=k&z=13&ie=UTF8&iwloc=&output=embed");
+        }
     }
+
+    
 }
