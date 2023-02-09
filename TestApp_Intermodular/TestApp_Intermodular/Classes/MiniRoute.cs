45222880cf62d36c6791753f607e2d4ee6a650d1
Sergio <sergio.suchpico@gmail.com>
2023-02-09 16:41:14 +0100

    Re-creando plantilla de rutas para mostrar

---
+++ TestApp_Intermodular/TestApp_Intermodular/Classes/MiniRoute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TestApp_Intermodular.Classes
+{
+    class MiniRoute
+    {
+        public static void ShowRoutes() 
+        {
+            Grid grid = new Grid
+            {
+                Height = 300,
+                Width = 300
+            };           
+        }
+    }   
+}
