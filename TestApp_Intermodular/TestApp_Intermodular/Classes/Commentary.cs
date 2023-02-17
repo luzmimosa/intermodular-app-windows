ee11c17068e01dc03294b2203f0c152a2981da26
Sergio <sergio.suchpico@gmail.com>
2023-02-17 19:41:53 +0100

    cambios generales menores

--- TestApp_Intermodular/TestApp_Intermodular/Classes/Commentary.cs
+++ TestApp_Intermodular/TestApp_Intermodular/Classes/Commentary.cs
@@ -1,16 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace TestApp_Intermodular.Classes
 {
-    internal class Commentary
+    public class Commentary
     {
-        public static string Content { get; set; }
-        public static string Date { get; set; }
-        public static string User { get; set; }
-        public static bool isAdmin { get; set; }
+        private string _content;
+        private DateTime _date;
+        private string _user;
+        private bool _isAdmin;
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public string User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+
+        public bool IsAdmin
+        {
+            get { return _isAdmin; }
+            set { _isAdmin = value; }
+        }
+
+        public bool CanEdit(string currentUser)
+        {
+            return _isAdmin || _user == currentUser;
+        }
     }
+
 }
+
+//< CUANDO SE VAYA A CREAR CADA INSTANCIA, AÑADIR ESTE BOTÓN EN EL C# DE CADA INSTANCIA CREADA
+//Button Content = "Edit" Visibility = "{Binding CanEdit, Converter={StaticResource BoolToVisibilityConverter}}"
+///>
+
